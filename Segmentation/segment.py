7d44101970d3f874a84dfa4dee9cfbd1966b0e4a
K7esavan <m.k7esavan@gmail.com>
2022-02-23 09:05:26 +0530

    permuted

--- Segmentation/segment.py
+++ Segmentation/segment.py
@@ -109,7 +109,7 @@ class PoseDataset(torch.utils.data.Dataset):
 
 def tensor_to_image(tensor):
     tensor = tensor*255
-    tensor = np.array(tensor, dtype=np.uint8)
+    tensor = np.array(tensor)
     if np.ndim(tensor)>3:
         assert tensor.shape[0] == 1
         tensor = tensor[0]
